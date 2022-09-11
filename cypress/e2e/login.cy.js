a4debbc765e0a54456c3b0e03d81e3124ee26ad5
Janaina Rodrigues <janarodrigues_95@hotmail.com>
2022-09-11 16:31:34 -0300

    Primeira alteracao no arquivo login

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,5 +1,23 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Funcionalidade Login', () => {
+  
+  beforeEach(() => {
+    cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    
+  });
+ 
+  it('Fazer login com sucesso', () => {
+   
   })
-})
+
+  it('Exibir mensagem de erro ao inserir usuário inválido', () => {
+   
+  })
+
+  it('Exibir mensagem de erro ao inserir senha inválida', () => {
+   
+  })
+
+  it('Recuperar senha perdida', () => {
+   
+  })
+});

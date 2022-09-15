7f0f31a3a17f85cfe7152c655da9e7db6bd5aa32
Janaina Rodrigues <janarodrigues_95@hotmail.com>
2022-09-14 21:17:15 -0300

    Alteracao arquivo Login

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,3 +1,5 @@
+/// <reference types="cypress" />
+
 describe('Funcionalidade Login', () => {
   
   beforeEach(() => {
@@ -6,18 +8,36 @@ describe('Funcionalidade Login', () => {
   });
  
   it('Fazer login com sucesso', () => {
-   
+    cy.get('#username').type("aluno@teste.com")
+    cy.get('#password').type("teste@123")
+    cy.get('.woocommerce-form > .button').click()
+
+    cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain' , 'Olá')
+
   })
 
   it('Exibir mensagem de erro ao inserir usuário inválido', () => {
-   
+    cy.get('#username').type("aluno50@teste.com")
+    cy.get('#password').type("teste@123")
+    cy.get('.woocommerce-form > .button').click()
+
+    cy.get('.woocommerce-error').should('contain' , 'Endereço de e-mail desconhecido')
+ 
   })
 
   it('Exibir mensagem de erro ao inserir senha inválida', () => {
-   
+    cy.get('#username').type("aluno@teste.com")
+    cy.get('#password').type("teste@4567")
+    cy.get('.woocommerce-form > .button').click()
+
+    cy.get('.woocommerce-error').should('contain' , 'Erro: A senha fornecida para o e-mail aluno@teste.com está incorreta. Perdeu a senha?')
+
   })
 
   it('Recuperar senha perdida', () => {
-   
+    cy.get('.lost_password > a').click()
+    cy.get('#user_login').type("aluno@teste.com")
+    cy.get('.woocommerce-Button').click()
+    cy.get('.woocommerce-message').should('contain' , 'O e-mail de redefinição de senha foi enviado')
   })
 });
